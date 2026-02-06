baaa18e99d9384bfd20b264846f244ff8d89f239
Mihir Patel <mihirpatel14@gmail.com>
2026-02-06 13:38:15 +0000

    Fix next config and disable lint during build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // <--- This is the magic line that creates the 'out' folder
-  eslint: {
-    // This allows the build to finish even if there are small linter warnings
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
 };
 
 export default nextConfig;
